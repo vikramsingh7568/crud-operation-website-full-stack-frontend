7858d736388e461c89c16d72fb48f7fa2a5b132c
Prince Soni <vikramsinghkhinchi5@gmail.com>
2023-03-04 16:34:00 +0530

    done

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
-const router = require("./router/route")
+const router = require("./src/router/route")
 const cors = require('cors')
 app.use(express.json())
 app.use(cors());
 
 app.use("/",router)
+mongoose.set('strictQuery', false);
 mongoose.connect("mongodb+srv://vikramsingh7568:AlLbBhXCJYPKmwIK@cluster0.5swhv4u.mongodb.net/e-commerce-website-fullstack?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
